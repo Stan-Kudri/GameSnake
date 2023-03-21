eac98790f1d169f611c8fcb3a889427770ab6022
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-03-21 22:48:49 +0300

    Changed responsibility from points to speed.

--- Snake/ComponentsGame/Speed.cs
+++ Snake/ComponentsGame/Speed.cs
@@ -2,16 +2,29 @@
 {
     public class Speed
     {
-        public const int ValueIncreaseSpeed = 20;
+        public const int ValueIncreaseSpeed = 25;
 
-        public Speed(int speed = 100)
+        private readonly int _startSpeed;
+        private readonly int _speedUpInterval;
+
+        private int _numberInterval = 0;
+
+        public Speed(int speed = 100, int speedUpInterval = 5)
         {
-            Value = speed;
+            Value = _startSpeed = speed;
+            _speedUpInterval = speedUpInterval;
         }
 
         public int Value { get; private set; }
 
-        public void Increase() => Value = Value - ValueIncreaseSpeed > 0 ? Value - ValueIncreaseSpeed : Value;
+        public void Increase(int score)
+        {
+            if (Value - ValueIncreaseSpeed > 0)
+            {
+                _numberInterval = score / _speedUpInterval;//Point interval number.
+                Value = _startSpeed - _numberInterval * ValueIncreaseSpeed;
+            }
+        }
 
         public void Apply() => Thread.Sleep(Value);
     }
