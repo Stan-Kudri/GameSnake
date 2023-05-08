ccfea3f3b8b6751fca9ce0961a785869b9c71c77
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-05-08 22:53:53 +0300

    SpeedMono - Rename _period -> _periodToRefreshMovement.

--- SnakeMonoGame/ComponentsGame/SpeedMono.cs
+++ SnakeMonoGame/ComponentsGame/SpeedMono.cs
@@ -6,7 +6,7 @@ namespace MonoGameSnake.ComponentsGame
     public class SpeedMono : Speed
     {
         private readonly int _buttonPressPeriod = 200;
-        private readonly int _period;
+        private readonly int _periodToRefreshMovement;
 
         public SpeedMono(int speed = 20, int thresholdPoints = 5, int valueIncreaseSpeed = 20, int maxSpeed = 100, int period = 5000)
             : base(speed, thresholdPoints, valueIncreaseSpeed, maxSpeed)
@@ -16,10 +16,10 @@ namespace MonoGameSnake.ComponentsGame
                 throw new ArgumentException("Period value is incorrect.", nameof(period));
             }
 
-            _period = period;
+            _periodToRefreshMovement = period;
         }
 
-        public int TimeMove => _period / Value;
+        public int TimeMove => _periodToRefreshMovement / Value;
 
         public int TimePressButton => _buttonPressPeriod / (_numberInterval + 1);
 
