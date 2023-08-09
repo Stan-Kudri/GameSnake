aaae88c9a06409443ae4af9bcf00bea2b56a5ae0
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2023-08-10 00:10:02 +0300

    GameSnake - Add constructor for testing.

--- GameSnake/ComponentsGame/ItemGameMap/SnakeConsole.cs
+++ GameSnake/ComponentsGame/ItemGameMap/SnakeConsole.cs
@@ -9,6 +9,11 @@ namespace GameSnake.ComponentsGame.ItemGameMap
         private const char SymbolSnake = 'О';
         private const int DividerLengthHalf = 2;
 
+        public SnakeConsole(int x, int y, Border border, int length = 1)
+            : base(x, y, border, length)
+        {
+        }
+
         public SnakeConsole(Border border, int length = StartLength, Directions directions = StartDirection)
             : base(
                   (border.Width / DividerLengthHalf) - length,
